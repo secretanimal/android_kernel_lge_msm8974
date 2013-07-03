8dc49bcb3151d7a120cafdef018aa0a2978e46ba
Lorenzo Colitti <lorenzo@google.com>
2013-07-04 00:52:49 +0900

    net: ipv6: add missing lock in ping_v6_sendmsg
    [msm/android-3.4 commit 3a018d2e8fc2910e5fb2077cde81cad659e2c7f9]
    
    Bug: 9469865
    Change-Id: I5ca9624d68fcbddf6469e5124a14b6fceedb7398
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/ping.c
+++ net/ipv6/ping.c
@@ -198,10 +198,7 @@ int ping_v6_sendmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 	if (hlimit < 0)
 		hlimit = ip6_dst_hoplimit(dst);
 
-/* 2013-11-25 hobbes.song LGP_DATA_CTS_IPV6_PINGTEST [START] */
 	lock_sock(sk);
-/* 2013-11-25 hobbes.song LGP_DATA_CTS_IPV6_PINGTEST [END] */
-
 	err = ip6_append_data(sk, ping_getfrag, &pfh, len,
 			      0, hlimit,
 			      np->tclass, NULL, &fl6, rt,
@@ -216,6 +213,7 @@ int ping_v6_sendmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 						 (struct icmp6hdr *) &pfh.icmph,
 						 len);
 	}
+	release_sock(sk);
 
 /* 2013-11-25 hobbes.song LGP_DATA_CTS_IPV6_PINGTEST [START] */
 	release_sock(sk);
