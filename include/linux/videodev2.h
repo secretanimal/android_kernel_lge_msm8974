607d9a4bc793f36f9219147637b9c814d6022371
Manikanta Sivapala <msivap@codeaurora.org>
2014-08-01 18:57:47 +0530

    msm: vidc: add new control for limiting i/p buffer size
    Take the minimum of the size calculated by driver using
    max width and height supported and the size set by client
    for input buffers. Change interface to get input and
    output buffer sizes.
    
    Change-Id: Ia3eb4cc7ae7bb38e2650fff1b694623e2aab62ef
    Signed-off-by: Manikanta Sivapala <msivap@codeaurora.org>

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -2039,6 +2039,9 @@ enum v4l2_mpeg_vidc_video_rate_control_timestamp_mode {
 #define V4L2_CID_MPEG_VIDC_VIDEO_BFRAME_Y_RANGE \
                 (V4L2_CID_MPEG_MSM_VIDC_BASE + 63)
 
+#define V4L2_CID_MPEG_VIDC_VIDEO_BUFFER_SIZE_LIMIT \
+		(V4L2_CID_MPEG_MSM_VIDC_BASE + 48)
+
 /*  Camera class control IDs */
 #define V4L2_CID_CAMERA_CLASS_BASE 	(V4L2_CTRL_CLASS_CAMERA | 0x900)
 #define V4L2_CID_CAMERA_CLASS 		(V4L2_CTRL_CLASS_CAMERA | 1)
